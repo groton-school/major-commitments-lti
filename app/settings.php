c6f4b2001c87cae8bce7a0fd0aeab2ff99c70526
Seth Battis <seth@battis.net>
2025-05-24 11:07:12 -0400

    refactor: rename LTI

--- app/settings.php
+++ app/settings.php
@@ -5,16 +5,20 @@ declare(strict_types=1);
 use App\Application\Settings\Settings;
 use App\Application\Settings\SettingsInterface;
 use DI\ContainerBuilder;
+use GrotonSchool\LTI\Registration\Instructure\Canvas\LtiConfiguration;
+use GrotonSchool\LTI\Registration\Instructure\Canvas\LtiMessage;
+use GrotonSchool\LTI\Registration\v1p0\OpenIDConfiguration;
 use Odan\Session\SessionInterface;
+use Packback\Lti1p3\LtiConstants;
 
 return function (ContainerBuilder $containerBuilder) {
 
     // Global Settings Object
     $containerBuilder->addDefinitions([
         SettingsInterface::class => function () {
-            $TOOL_NAME = 'Planner LTI';
+            $TOOL_NAME = 'Major Commitments LTI';
             $PROJECT_URL = 'https://' . getenv('HTTP_HOST');
-            $SCOPES = ['https://purl.imsglobal.org/spec/lti-nrps/scope/contextmembership.readonly'];
+            $SCOPES = [LtiConstants::NRPS_SCOPE_MEMBERSHIP_READONLY];
             return new Settings([
                 'displayErrorDetails'   => false,
                 'logError'              => true,
@@ -29,37 +33,36 @@ return function (ContainerBuilder $containerBuilder) {
                 Settings::CACHE_DURATION => 3600, // seconds
                 Settings::REDIRECT_URI => "{$PROJECT_URL}/login/oauth2/redirect",
                 Settings::TOOL_REGISTRATION => [
-                    'application_type' => 'web',
-                    'client_name' => $TOOL_NAME,
-                    'client_uri' => $PROJECT_URL,
-                    'grant_types' => ['client_credentials', 'implicit'],
-                    'jwks_uri' => "{$PROJECT_URL}/lti/jwks",
-                    'token_endpoint_auth_method' => 'private_key_jwt',
-                    'initiate_login_uri' =>  "{$PROJECT_URL}/lti/login",
-                    'redirect_uris' => ["{$PROJECT_URL}/lti/launch"],
-                    'response_types' => ['id_token'],
-                    "scope" => join(' ', $SCOPES),
-                    'https://purl.imsglobal.org/spec/lti-tool-configuration' => [
-                        'domain' => preg_replace('@^https?://@', '', $PROJECT_URL),
-                        'target_link_uri' => "{$PROJECT_URL}/lti/launch",
-                        'messages' => [
+                    OpenIDConfiguration::APPLICATION_TYPE => OpenIDConfiguration::APPLICATION_TYPE_WEB,
+                    OpenIDConfiguration::CLIENT_NAME => $TOOL_NAME,
+                    OpenIDConfiguration::CLIENT_URI => $PROJECT_URL,
+                    OpenIDConfiguration::GRANT_TYPES => OpenIDConfiguration::GRANT_TYPES_REQUIRED,
+                    OpenIDConfiguration::JWKS_URI => "{$PROJECT_URL}/lti/jwks",
+                    OpenIDConfiguration::TOKEN_ENDPOINT_AUTH_METHOD => 'private_key_jwt',
+                    OpenIDConfiguration::INITIATE_LOGIN_URI =>  "{$PROJECT_URL}/lti/login",
+                    OpenIDConfiguration::REDIRECT_URIS => ["{$PROJECT_URL}/lti/launch"],
+                    OpenIDConfiguration::RESPONSE_TYPES => [OpenIDConfiguration::RESPONSE_TYPE_ID_TOKEN],
+                    OpenIDConfiguration::SCOPE => join(' ', $SCOPES),
+                    OpenIDConfiguration::LTI_TOOL_CONFIGURATION => [
+                        LtiConfiguration::DOMAIN => parse_url($PROJECT_URL, PHP_URL_HOST),
+                        LtiConfiguration::TARGET_LINK_URI => "{$PROJECT_URL}/lti/launch",
+                        LtiConfiguration::MESSAGES => [
                             [
-                                "type" => "LtiResourceLinkRequest",
-                                "icon_uri" => "{$PROJECT_URL}/assets/icon.svg",
-                                "label" => "Planner",
-                                "custom_parameters" => [
+                                LtiMessage::TYPE => LtiMessage::TYPE_RESOURCE,
+                                LtiMessage::ICON_URI => "{$PROJECT_URL}/assets/icon.svg",
+                                LtiMessage::LABEL => "Major Commitments",
+                                LtiMessage::CUSTOM_PARAMETERS => [
                                     'user_id' => '$Canvas.user.id',
                                     'brand_config_json_url' => '$com.instructure.brandConfigJSON.url',
                                     'brand_config_js_url' => '$com.instructure.brandConfigJS.url',
                                     'common_css_url' => '$Canvas.css.common',
-                                    'prefers_high_contrast' => '$Canvas.user.prefersHighContrast'
+                                    'prefers_high_contrast' => '$Canvas.user.prefersHighContrast',
                                 ],
-                                "placements" => ["global_navigation"],
-                                "roles" => [],
-                                "target_link_uri" => "{$PROJECT_URL}/lti/launch?placement=global_navigation"
+                                LtiMessage::PLACEMENTS => [LtiMessage::PLACEMENT_COURSE_NAVIGATION],
+                                LtiMessage::ROLES => [LtiConstants::MEMBERSHIP_INSTRUCTOR],
                             ]
                         ],
-                        "claims" => [
+                        LtiConfiguration::CLAIMS => [
                             "sub",
                             "iss",
                             "name",
@@ -70,12 +73,12 @@ return function (ContainerBuilder $containerBuilder) {
                             "email",
                             "locale"
                         ],
-                        'https://canvas.instructure.com/lti/privacy_level' => 'public'
+                        LtiConfiguration::PRIVACY_LEVEL => LtiConfiguration::PRIVACY_PUBLIC
 
                     ]
                 ],
                 SessionInterface::class => [
-                    'name' => "planner-session",
+                    'name' => "major-commitments-session",
                     'lifetime' => 60 * 60 * 24,
                     'cookie_samesite' => 'None',
                     'secure' => true,
